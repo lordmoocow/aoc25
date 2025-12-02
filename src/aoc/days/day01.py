69d0789eb2d7ac3c35813c29b101cf8b9e57d0dc
lordmoocow <src@samueljwilliams.co.uk>
2025-12-02 00:39:34 +0000

    password method 0x434C49434B

--- src/aoc/days/day01.py
+++ src/aoc/days/day01.py
@@ -5,34 +5,54 @@ def read_instructions(input_data) -> list[int]:
     return [rotate(line) for line in lines]
 
 def rotate(instruction: str) -> int:
-    """Read instruction line"""
-    i: int = int(instruction[1:])
-    if instruction[0] == "R":
-        return i
-    else:
-        return -i
+    i = int(instruction[1:])
+    return i if instruction[0] == "R" else -i
 
-def part1(input_data: str) -> int | str:
-    """The Safe is a Decoy"""
+def apply(selected: int, operation: int) -> int:
+    # Rotate in direction of operation (- goes backwards)
+    selected += operation
+    # Mod 100 will wrap values around 0-99
+    selected %= 100
+    return selected
 
+def part1(input_data: str) -> int:
+    """The Password"""
     # Read each line and determine rotation direction R=+/L=-
     operations = read_instructions(input_data)
-
     # Track "0" count for result
-    count: int = 0
-
+    count = 0
     # Start at position 50
-    selected: int = 50
+    selected = 50
 
     for op in operations:
-        # Rotate in direction of operation (- goes backwards)
-        selected += op
-
-        # Mod 100 will wrap values values around 0-99
-        selected %= 100
-
-        # Check if we are at 0 exactly
+        selected = apply(selected, op)
         count += selected == 0
 
     return count
 
+def part2(input_data: str) -> int:
+    """Password Method 0x434C49434B"""
+    # Read each line and determine rotation direction R=+/L=-
+    operations = read_instructions(input_data)
+    # Track "0" count for result
+    count = 0
+    # Start at position 50
+    selected = 50
+
+    for op in operations:
+        new_position = apply(selected, op)
+        
+        # Count full rotations
+        count += abs(int(op / 100))
+        # Ignore partial rotation if we _start_ on 0 as we would have already counted it
+        if selected != 0:
+            # Compare new dial position to the original position and direction of rotation
+            if (op > 0 and new_position < selected) or (op < 0 and new_position > selected):
+                count += 1
+            # Or did we end on 0 exactly
+            elif new_position == 0:
+                count += 1
+
+        selected = new_position
+
+    return count
