d44cd0725b8c37c9fd9a2334f0e8c3ab1c9cb8f0
lordmoocow <src@samueljwilliams.co.uk>
2025-12-03 22:16:59 +0000

    joltage saftey override

--- src/aoc/days/day03.py
+++ src/aoc/days/day03.py
@@ -27,9 +27,32 @@ def joltage(bank: str) -> int:
     return int(j + j2)
                
 
+
+def joltage2(bank: str, cells: int) -> int:
+    joltage = ""
+    index = 0
+    # repeate scan for the desired number of cells
+    # always need to leave enough remaining cells so count down as we scan
+    for x in range(cells-1, -1, -1):
+        # scan for next highest number in remaining cells after last one
+        # leave enough cells at the end to complete all
+        j, i = scan(bank[index:len(bank)-x])
+        # i is only relative to the sub str so add it to the full str index
+        index += i + 1
+        joltage += j
+    return int(joltage)
+
 def part1(input_data: str) -> int:
     """Maximum Joltage"""
     battery_banks = load_battery_banks(input_data)
     return sum(
         joltage(i) for i in battery_banks
+    )
+
+
+def part2(input_data: str) -> int:
+    """Joltage Safety Override""" 
+    battery_banks = load_battery_banks(input_data)
+    return sum(
+        joltage2(i, 12) for i in battery_banks
     )
