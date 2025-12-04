ad96c605bc3d2e25b53cb4696674e2fc30cff76a
lordmoocow <src@samueljwilliams.co.uk>
2025-12-04 19:37:28 +0000

    cleanup in the lobby

--- src/aoc/days/day03.py
+++ src/aoc/days/day03.py
@@ -2,36 +2,24 @@
 
 
 def load_battery_banks(input_data: str) -> list[str]:
-    lines = input_data.strip().split("\n")
-    return [line for line in lines]
+    return input_data.strip().split("\n")
 
 
 def scan(bank: str) -> tuple[str, int]:
-    index, joltage = 0, 0
-    for i, cell in enumerate(bank):
-        cell = int(cell)
-
-        # does this sell have a higher joltage?
-        # track which index it was at for next scan
-        if cell > joltage:
-            joltage = cell
-            index = i
-
-    # return cell joltage and it's bank index
-    return (str(joltage), index)
+    index, joltage = max(enumerate(bank), key=lambda x: int(x[1]))
+    return joltage, index
 
 
 def joltage(bank: str) -> int:
     j, i = scan(bank[:-1])
     j2, _ = scan(bank[i + 1:])
     return int(j + j2)
-               
 
 
 def joltage2(bank: str, cells: int) -> int:
-    joltage = ""
+    digits = []
     index = 0
-    # repeate scan for the desired number of cells
+    # repeat scan for the desired number of cells
     # always need to leave enough remaining cells so count down as we scan
     for x in range(cells-1, -1, -1):
         # scan for next highest number in remaining cells after last one
@@ -39,8 +27,9 @@ def joltage2(bank: str, cells: int) -> int:
         j, i = scan(bank[index:len(bank)-x])
         # i is only relative to the sub str so add it to the full str index
         index += i + 1
-        joltage += j
-    return int(joltage)
+        digits.append(j)
+    return int("".join(digits))
+
 
 def part1(input_data: str) -> int:
     """Maximum Joltage"""
@@ -51,7 +40,7 @@ def part1(input_data: str) -> int:
 
 
 def part2(input_data: str) -> int:
-    """Joltage Safety Override""" 
+    """Joltage Safety Override"""
     battery_banks = load_battery_banks(input_data)
     return sum(
         joltage2(i, 12) for i in battery_banks
