40bd98b3b9a0b05a318fa67746a103076387d943
lordmoocow <src@samueljwilliams.co.uk>
2025-12-04 23:42:08 +0000

    cleanup in the printing room

--- src/aoc/days/day04.py
+++ src/aoc/days/day04.py
@@ -1,19 +1,23 @@
 """Advent of Code 2025 - Day 04."""
 
+
+from itertools import product
+
+
 def load_diagram(input_data: str) -> list[list[str]]:
     return [[x for x in line] for line in input_data.strip().splitlines()]
 
 
-def accessible(diagram: list[list[str]], x, y: int) -> bool:
-    count = 0
-    for a in range(-1, 2):
-        if x+a < 0 or x+a >= len(diagram):
-            continue
-        for b in range(-1, 2):
-            if y+b < 0 or y+b >= len(diagram[0]):
-                continue
-            if not (a == b == 0) and diagram[x+a][y+b] == "@":
-                count += 1
+def accessible(diagram: list[list[str]], x: int, y: int) -> bool:
+    height, width = len(diagram), len(diagram[0])
+    count = sum(
+        1
+        for dx, dy in product(range(-1, 2), repeat=2)
+        if (dx, dy) != (0, 0)
+        and 0 <= x + dx < height
+        and 0 <= y + dy < width
+        and diagram[x + dx][y + dy] == "@"
+    )
     return count < 4
 
 
@@ -21,7 +25,7 @@ def part1(input_data: str) -> int:
     """Accessible Forklifts"""
     diagram = load_diagram(input_data)
     return sum(
-        1 
+        1
         for x, row in enumerate(diagram)
         for y, cell in enumerate(row)
         if cell == "@" and accessible(diagram, x, y)
@@ -30,20 +34,15 @@ def part1(input_data: str) -> int:
 
 def part2(input_data: str) -> int:
     """Shifting Paper"""
-    diagram = load_diagram(input_data) 
-    removable = []
+    diagram = load_diagram(input_data)
     removed = 0
-    while True:
-        removable = [
-            (x,y) 
-            for x, row in enumerate(diagram)
-            for y, cell in enumerate(row)
-            if cell == "@" and accessible(diagram, x, y)
-        ]
-        if len(removable) <= 0:
-            break
-        for rollX, rollY in removable:
-            diagram[rollX][rollY] = "."
+    while removable := [
+        (x, y)
+        for x, row in enumerate(diagram)
+        for y, cell in enumerate(row)
+        if cell == "@" and accessible(diagram, x, y)
+    ]:
+        for roll_x, roll_y in removable:
+            diagram[roll_x][roll_y] = "."
         removed += len(removable)
     return removed
-        
